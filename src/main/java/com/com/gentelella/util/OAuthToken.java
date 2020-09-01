b56a3ec5cff83dd46c3efa78534b9f0782141c4c
Dongok Lee <orm6711@gmail.com>
2020-09-01 14:21:23 +0900

    토큰 정보 맵핑

---
+++ src/main/java/com/com/gentelella/util/OAuthToken.java
@@ -0,0 +1,51 @@
+package com.com.gentelella.util;
+
+import lombok.Data;
+
+
+public class OAuthToken {
+
+	private String access_token;
+	public String getAccess_token() {
+		return access_token;
+	}
+	public String getToken_type() {
+		return token_type;
+	}
+	public void setToken_type(String token_type) {
+		this.token_type = token_type;
+	}
+	public String getRefresh_token() {
+		return refresh_token;
+	}
+	public void setRefresh_token(String refresh_token) {
+		this.refresh_token = refresh_token;
+	}
+	public int getExpires_in() {
+		return expires_in;
+	}
+	public void setExpires_in(int expires_in) {
+		this.expires_in = expires_in;
+	}
+	public String getScope() {
+		return scope;
+	}
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+	public int getRefresh_token_expires_in() {
+		return refresh_token_expires_in;
+	}
+	public void setRefresh_token_expires_in(int refresh_token_expires_in) {
+		this.refresh_token_expires_in = refresh_token_expires_in;
+	}
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+	private String token_type;
+	private String refresh_token;
+	private int expires_in;
+	private String scope;
+	private int refresh_token_expires_in;
+	
+}
