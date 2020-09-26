bc6bec55f65297c6e594c9fbeb95fda3ae47d9e6
Dongok Lee <orm6711@gmail.com>
2020-09-26 16:27:56 +0900

    TDD구동테스트, JUnit5.ver

--- src/test/java/com/com/boot/OauthTest.java
+++ src/test/java/com/com/boot/OauthTest.java
@@ -1,5 +1,18 @@
 package com.com.boot;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class OauthTest {
 
+	
+	@Test
+	public void testKakaoInfo() {
+		//기존 데이터 테스트 코드에 앞서  JUnit 동작관련 테스트  파일 전부 재구성예정
+		Calculator calculator = new Calculator();
+		assertThat(calculator.plus(1,2), is(3));
+		
+	}
 }
